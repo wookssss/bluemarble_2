53c485918595e7b97570ed0a4b6dd752ac73585f
Lee Seungjong <lsjjong8@naver.com>
2024-08-20 11:28:13 +0900

    기본 파일 커밋

---
+++ src/main/java/itschool/bluemarble/Application.java
@@ -0,0 +1,7 @@
+package itschool.bluemarble;
+
+public class Application {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/itschool/bluemarble/enumclass/Color.java
@@ -0,0 +1,5 @@
+package itschool.bluemarble.enumclass;
+
+public enum Color {
+    PINK, GREEN, BROWN, NAVY
+}
