0d3e40eb526a020557123de117daf2a01e09dc39
Lee Seungjong <lsjjong8@naver.com>
2024-08-20 13:16:12 +0900

    플레이어에 move method(절대, 상대) 추가

--- src/main/java/itschool/bluemarble/enttiy/Player.java
+++ src/main/java/itschool/bluemarble/enttiy/Player.java
@@ -1,10 +1,26 @@
 package itschool.bluemarble.enttiy;
 
 public class Player {
+    // 현재 위치 : 타일 번호 0 ~ 39
+    private int curPos = 0;
 
     private Dice dice = new Dice();
 
     public int rollDice() {
         return dice.roll();
+    }
+
+    // 절대적인 타일번호
+    public void moveByAbsoluteValue(int abs) {
+        curPos = abs;
+    }
+
+    // 상대적으로 변화값
+    public void moveByRelativeValue(int rel) {
+        curPos += rel;
+
+        if(curPos > 39) {
+            curPos -= 39;
+        }
     }
 }
