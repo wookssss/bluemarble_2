7e065f287e72251ecc30484727fb6cbdbdd7b352
OHJAEHEON <mykd2012@gmail.com>
2024-08-20 15:54:39 +0900

    주사위 roll메서드 구현

--- src/main/java/itschool/bluemarble/entity/Dice.java
+++ src/main/java/itschool/bluemarble/entity/Dice.java
@@ -1,16 +1,38 @@
-package itschool.bluemarble.entity;
+package itschool.bluemarble.enttiy;
 
 import java.util.Random;
 
 public class Dice {
-    int dice1;
-    int dice2;
+    private int dice1;
+    private int dice2;
+    private static int dice_result = 0;
+    private static int Double_count = 0;
+
+
 
     public int roll() { // 주사위 2개를 굴린다. 2 ~ 12
+        if (Double_count == 0) {dice_result = 0;} // 새로운 플레이어가 주사위를 굴릴경우 결과를 0으로 초기화후 주사위 굴리기
         dice1 = 1 + new Random().nextInt(6);
         dice2 = 1 + new Random().nextInt(6);
+        dice_result = dice1 + dice2 + dice_result; // 이전에 더블이 있었을 경우 결과 값을 누적
 
-        return dice1 + dice2;
+        System.out.println("dice1: " + dice1);
+        System.out.println("dice2: " + dice2);
+
+        if (isDouble()) {
+            if (Double_count < 3) {
+                System.out.println("더블! 한번더!");
+                Double_count++;
+                return 0; // 더블이 나오면 0을 리턴
+            }else{
+                System.out.println("더블이 3회입니다. 무인도로 이동!");
+                return 1; // 더블이 3회나올 경우 1을 리턴
+            }
+        }else { //
+            Double_count = 0;
+        }
+
+        return dice_result;
     }
 
     public boolean isDouble() {
