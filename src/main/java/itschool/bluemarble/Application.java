e4c47fb5063b9f7a3a211c9ecd2bcfda2212dc0d
Seungjong Lee <lsjjong8@naver.com>
2024-08-20 19:35:38 +0900

    Application의 메인 메서드 수정 (Game 시작을 위한 콘솔 입력 요청, 게임 생성 및 시작)

--- src/main/java/itschool/bluemarble/Application.java
+++ src/main/java/itschool/bluemarble/Application.java
@@ -1,7 +1,25 @@
 package itschool.bluemarble;
 
+import itschool.bluemarble.progress.GameScreen;
+
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        int numberOfPlayer; // 기본값 2인
+
+        do {
+            System.out.print("플레이어 수를 입력하세요 : ");
+            numberOfPlayer = sc.nextInt();
+            // 플레이어 이름은 글자 수를 고정해야함.. 콘솔의 단점
+        } while(numberOfPlayer >= 2 && numberOfPlayer <= 4);
+
+        GameScreen game = GameScreen.createGame(numberOfPlayer);
+
+        if(null != game) {
+            game.start();
+        }
     }
 }
