ede29b41951891f31d678723e88ebda5320ad2bd
Lee seunghwan <keynes@naver.com>
2024-08-21 18:17:34 +0900

    황금열쇠 이름 및 서울로 이동 추가

--- src/main/java/itschool/bluemarble/factory/GoldenKeys.java
+++ src/main/java/itschool/bluemarble/factory/GoldenKeys.java
@@ -25,12 +25,14 @@ public class GoldenKeys extends Tile {
     // 인스턴스 초기화 블록
     {
         goldenKeyList.addAll(Arrays.asList(
-                new Goldenkey("뒤로 2칸 이동", (InstantFunction) ((player) -> player.moveByRelativeValue(-2))//, // 뒤로 2칸 이동
-                /*(InstantKey) ((player) -> player.moveByRelativeValue(-3)), // 뒤로 3칸 이동
+                new Goldenkey("뒤로 2칸 이동", (InstantFunction) ((player) -> player.moveByRelativeValue(-2))), // 뒤로 2칸 이동
+                new Goldenkey("뒤로 2칸 이동", (InstantFunction) ((player) -> player.moveByRelativeValue(-3))), // 뒤로 3칸 이동
+                new Goldenkey("서울로 이동", (InstantFunction) ((player) -> player.moveByAbsoluteValue(39))) // 뒤로 3칸 이동
+        ));
+        /*(InstantKey) ((player) -> player.moveByRelativeValue(-3)), // 뒤로 3칸 이동
                 (InstantKey) ((player) -> player.moveByAbsoluteValue(39)), // 서울로 이동 황금열쇠
                 // (HoldableKey) // 뭐가 있을까?
                 new TollFreePassKey() // 통행료 면제*/
-        )));
     }
 
 }
