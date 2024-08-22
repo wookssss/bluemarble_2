057c5b4e25179cc0bbe1c4a0fa9e6219ce3bdb5c
OHJAEHEON <mykd2012@gmail.com>
2024-08-22 13:51:03 +0900

    Game및 Dice 결과 출력 수정

--- src/main/java/itschool/bluemarble/entity/Dice.java
+++ src/main/java/itschool/bluemarble/entity/Dice.java
@@ -9,25 +9,34 @@ import java.util.Random;
 
 @Getter
 public class Dice {
-    private int dice1; // 주사위1
-    private int dice2; // 주사위2
+    private int dice1 = 0; // 주사위1
+    private int dice2 = 0; // 주사위2
     private static int doubleCount = 0; // 더블의 횟수를 가지고 있는 변수
 
     public int roll() { // 주사위 2개를 굴린다. 2 ~ 12
         dice1 = 1 + new Random().nextInt(6);
         dice2 = 1 + new Random().nextInt(6);
 
-        System.out.println("dice1: " + dice1);
-        System.out.println("dice2: " + dice2);
-
         if (isDouble()) {
-                System.out.println("더블! 한번더!");
                 doubleCount++;
         }else { //
             doubleCount = 0;
         }
-
         return dice1 + dice2;
+    }
+
+    public int getDoubleCount() {
+        return doubleCount;
+    }
+
+    public String toresultString(String name) {
+        String result = "dice1=" + dice1 +
+                "\ndice2=" + dice2;
+
+        if (isDouble()) {result += "\n더블입니다.";}
+        result += "\n" + name + "님의 주사위 값 : " + (dice1 + dice2);
+
+        return  result;
     }
 
     public boolean isDouble() {
