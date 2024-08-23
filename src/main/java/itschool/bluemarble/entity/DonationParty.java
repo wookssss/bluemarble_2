564faea879a9bae8b5fe6d44c9b037df1794a46d
100 Jihye <jejeje0431@gmail.com>
2024-08-23 10:22:56 +0900

    DonationParty 싱글톤 추가

--- src/main/java/itschool/bluemarble/entity/DonationParty.java
+++ src/main/java/itschool/bluemarble/entity/DonationParty.java
@@ -5,6 +5,14 @@ import itschool.bluemarble.entity.ifs.Payable;
 public class DonationParty implements Payable {
     private int curMoney;
 
+    // 싱글톤 시작
+    final private static DonationParty instance = new DonationParty();
+    public static DonationParty getInstance() {
+        return instance;
+    }
+    private DonationParty() { this.curMoney = 0; }
+    // 싱글톤 끝
+
     public void pay(Payable receiver, int amount){
         receiver.income(curMoney);
         curMoney = 0;
