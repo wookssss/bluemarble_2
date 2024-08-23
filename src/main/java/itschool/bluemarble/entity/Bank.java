848198c8bfaab6c5433c0404403760b32fd87e43
100 Jihye <jejeje0431@gmail.com>
2024-08-23 10:18:42 +0900

    Bank 싱클톤 추가 Player 땅구매, 반값판매 추가 Payable 수정

--- src/main/java/itschool/bluemarble/entity/Bank.java
+++ src/main/java/itschool/bluemarble/entity/Bank.java
@@ -1,9 +1,19 @@
 package itschool.bluemarble.entity;
 
 import itschool.bluemarble.entity.ifs.Payable;
+import itschool.bluemarble.factory.GoldenKeyTile;
 
 public class Bank implements Payable {
-    private int curMoney;
+    // 싱글톤 시작
+    final private static Bank instance = new Bank();
+    public static Bank getInstance() {
+        return instance;
+    }
+    private Bank() {}
+    // 싱글톤 끝
+
+
+    private int curMoney = 100_000_000;
 
     public void pay(Payable receiver, int amount){
         if(curMoney >= amount){
--- src/main/java/itschool/bluemarble/entity/ifs/Payable.java
+++ src/main/java/itschool/bluemarble/entity/ifs/Payable.java
@@ -1,7 +1,7 @@
 package itschool.bluemarble.entity.ifs;
 
 public interface Payable {
-    void pay(Payable receiver, int amount);
+    void pay(Payable receiver, int amount) throws Exception;
 
     void income(int amount);
 }
