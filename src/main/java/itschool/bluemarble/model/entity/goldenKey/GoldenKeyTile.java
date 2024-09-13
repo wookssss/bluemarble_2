19b2073b3470a43ec228268a6ad6758d14a0a21d
Lee Seungjong <lsjjong8@naver.com>
2024-09-13 15:12:36 +0900

    GoldenKeyTile의 이동 function 명칭을 MoveFunction으로 변경

--- src/main/java/itschool/bluemarble/model/entity/goldenKey/GoldenKeyTile.java
+++ src/main/java/itschool/bluemarble/model/entity/goldenKey/GoldenKeyTile.java
@@ -11,6 +11,7 @@ package itschool.bluemarble.model.entity.goldenKey;
 
 import itschool.bluemarble.exception.violation.PlayerHasNoLandViolation;
 import itschool.bluemarble.model.entity.Player;
+import itschool.bluemarble.model.entity.goldenKey.ifs.MoveFunction;
 import itschool.bluemarble.model.entity.tile.Tile;
 import itschool.bluemarble.model.entity.goldenKey.ifs.InstantFunction;
 import itschool.bluemarble.progress.GameByConsole;
@@ -40,18 +41,18 @@ public class GoldenKeyTile extends Tile {
     private static void initializeGoldenKeys() {
         goldenKeyList.addAll(Arrays.asList(
                 // 이동 시작 (9종)
-                new GoldenKey("고속도로", "출발지로 이동합니다.",(InstantFunction) ((player) -> player.moveByAbsoluteValue(0))),
-                new GoldenKey("관광여행", "부산으로 이동합니다.",(InstantFunction) ((player) -> player.moveByAbsoluteValue(25))),
-                new GoldenKey("관광여행", "서울로 이동합니다.",(InstantFunction) ((player) -> player.moveByAbsoluteValue(39))),
-                new GoldenKey("관광여행", "제주도로 이동합니다.",(InstantFunction) ((player) -> player.moveByAbsoluteValue(5))),
-                new GoldenKey("무인도", "무인도로 이동합니다.",(InstantFunction) ((player) -> player.moveByAbsoluteValue(10))),
-                new GoldenKey("사회복지기금 배당", "사회복지기금으로 이동합니다.",(InstantFunction) ((player) -> player.moveByAbsoluteValue(20))),
+                new GoldenKey("고속도로", "출발지로 이동합니다.",(MoveFunction) ((player) -> player.moveByAbsoluteValue(0))),
+                new GoldenKey("관광여행", "부산으로 이동합니다.",(MoveFunction) ((player) -> player.moveByAbsoluteValue(25))),
+                new GoldenKey("관광여행", "서울로 이동합니다.",(MoveFunction) ((player) -> player.moveByAbsoluteValue(39))),
+                new GoldenKey("관광여행", "제주도로 이동합니다.",(MoveFunction) ((player) -> player.moveByAbsoluteValue(5))),
+                new GoldenKey("무인도", "무인도로 이동합니다.",(MoveFunction) ((player) -> player.moveByAbsoluteValue(10))),
+                new GoldenKey("사회복지기금 배당", "사회복지기금으로 이동합니다.",(MoveFunction) ((player) -> player.moveByAbsoluteValue(20))),
                 // new GoldenKey("세계일주 초대권", "현재 위치에서 현재 위치로 한 바퀴 돌아오며, 월급 및 사회복지기금을 받을 수 있다.", ),
-                new GoldenKey("우주여행 초청장", "우주여행으로 이동합니다.",(InstantFunction) ((player) -> player.moveByAbsoluteValue(30))),
+                new GoldenKey("우주여행 초청장", "우주여행으로 이동합니다.",(MoveFunction) ((player) -> player.moveByAbsoluteValue(30))),
                 // new GoldenKey("유람선 여행", "우주여행 칸으로 간다. (황금열쇠로 이동하는 경우 소유자가 있어도 무료 이용 가능하다.)",),
-                new GoldenKey("이사", "뒤로 두 칸 이동합니다.",(InstantFunction) ((player) -> player.moveByRelativeValue(-2))),
-                new GoldenKey("이사","뒤로 세 칸 이동합니다." ,(InstantFunction) ((player) -> player.moveByRelativeValue(-3))),
-                new GoldenKey("특정 장소로 이동","원하는 곳으로 이동 가능하다. (이동은 한 방향으로 고정되어 있기에 이동하는 길에 출발지가 있으면 월급을 받는다.)", (InstantFunction) (player) -> player.moveByAbsoluteValue(GameByConsole.requestTileIndex(player))),
+                new GoldenKey("이사", "뒤로 두 칸 이동합니다.",(MoveFunction) ((player) -> player.moveByRelativeValue(-2))),
+                new GoldenKey("이사","뒤로 세 칸 이동합니다." ,(MoveFunction) ((player) -> player.moveByRelativeValue(-3))),
+                new GoldenKey("특정 장소로 이동","원하는 곳으로 이동 가능하다. (이동은 한 방향으로 고정되어 있기에 이동하는 길에 출발지가 있으면 월급을 받는다.)", (MoveFunction) (player) -> player.moveByAbsoluteValue(GameByConsole.requestTileIndex(player))),
                 // new GoldenKey("항공여행","콩코드 여객기를 타고 타이베이로 이동(주인이 있을 경우 통행료 지불 여객기, 건물 주인)" ,),
                 // 이동 끝
 
